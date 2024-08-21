fe897c0441388d2c1592f2391d18b2e1b59a73fd
James Woods <james.woods@neotechnology.com>
2024-08-21 16:31:57 +0100

    Removed validation for cleint ID and secret as they are variable

--- google_creator.py
+++ google_creator.py
@@ -69,51 +69,16 @@ def main_func(sso_button_text):
     
 #function that accepts user input, calls another function to validate the input
 def set_client_id():
-    while True:
-        #prompts user to enter their client id and validates if the input is a valid google client ID
-        client_id  = input("Please enter your Google Client ID, for example: 729287977690-d1kmti260aebur3bl06vag30i49vogi5.apps.googleusercontent.com")
-        #calls validate_client_id function to get a boolean value, true if its valid, false if it is not
-        is_valid_client_id = validate_client_id(client_id)
-        if is_valid_client_id == True:
-            return client_id
-        #calls validate_client_id, if the ID is invalid, asks user to enter valid client ID and loops the function
-        if is_valid_client_id == False:
-            print("Please enter a valid Client ID")
+    #promps user to enter their client id 
+    client_id  = input("Please enter your Google Client ID, for example: 729287977690-d1kmti260aebur3bl06vag30i49vogi5.apps.googleusercontent.com")
+    return client_id
 
 def set_client_secret():
-    #loops indefinitely until a valid value is entered
-    while True:
-        #prompts user to enter their client secret
-        client_secret = input("Please enter client secret:")
-        #calls the validate_client_secret function which returns a boolean value
-        valid_secret = validate_client_secret(client_secret)
-        # if the secret is valid returns it
-        if valid_secret == True:
-            return client_secret
-        # if the secret is invalid, tells the user and then loops the function
-        if valid_secret == False:
-            print("Invalid secret entered")
-            
-def validate_client_secret(client_secret):
-    # tries to match the format of a valid google auth secret key
-    is_client_secret_valid = re.match("GOCSPX-[A-Za-z0-9]{5}-[A-Za-z0-9]{22}$", client_secret)
-    # if there is a match the value is a match object, and is not None so is_client_secret_valid is set to true
-    if is_client_secret_valid is not None:
-        is_client_secret_valid = True
-    # if there is no match it will be None, so we set is_client_secret_valid to false
-    else:
-        is_client_secret_valid = False
-    return is_client_secret_valid
+
+    #prompts user to enter their client secret
+    client_secret = input("Please enter client secret:")
+    return client_secret
     
-#a function that uses regex to check the format of the client ID, google is always 8 numbers, followed by a dash and then 32 alphanumeric characters, the rest is static.
-def validate_client_id(client_id):
-    #re.match function returns None if there is no match and a match object if there is a match
-    is_client_id_valid = re.match("[0-9]{12}-[A-Za-z0-9]{32}\.apps\.googleusercontent\.com$", client_id)
-    #if is_client_id_valid is not None, it means there was a match, which means the value is valid
-    if is_client_id_valid is not None:
-        is_client_id_valid = True
-    #if is_guid_valid is None this means the guid is invalid, so it sets is_guid_valid to false
-    else:
-        is_client_id_valid = False
-    return is_client_id_valid
+        
+            
 
